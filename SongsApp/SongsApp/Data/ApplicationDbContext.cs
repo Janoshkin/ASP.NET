eee61da650a1268ad43b23d9555dd3872bac5e6f
Janna Niemi <N2927@student.jamk.fi>
2021-11-12 13:06:12 +0200

    Added Songs project

---
+++ SongsApp/SongsApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SongsApp.Models;
+
+namespace SongsApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SongsApp.Models.Song> Song { get; set; }
+    }
+}
---
+++ SongsApp/SongsApp/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongsApp.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string SongTitle { get; set; }
+        public string SongLyrics { get; set; }
+
+        public Song()
+        {
+
+        }
+
+    }
+}
